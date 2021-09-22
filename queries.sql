c75a52461fc87498632fc4f1e915fe9f95c3ad06
unknown <jchernandez827@gmail.com>
2021-09-22 10:12:58 -0500

    Add new queries

--- queries.sql
+++ queries.sql
@@ -7,3 +7,43 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+-- Transactions
+BEGIN;
+UPDATE animals SET species = 'unespecified';
+ROLLBACK;
+BEGIN;
+UPDATE animals SET species ='digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species ='pokemon' WHERE species IS NULL;
+COMMIT;
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT sp1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO sp1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
+-- Num of animals
+SELECT COUNT(*) FROM animals;
+
+-- Never tried to escape
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+-- Avg weight
+SELECT AVG(weight_kg) FROM animals;
+
+-- Escape the most
+SELECT MAX(escape_attempts) FROM animals WHERE neutered = true OR neutered = false;
+
+-- Max and min weight of each species
+SELECT MAX(weight_kg) FROM animals WHERE species = 'digimon';
+SELECT MIN(weight_kg) FROM animals WHERE species = 'digimon';
+SELECT MAX(weight_kg) FROM animals WHERE species = 'pokemon';
+SELECT MIN(weight_kg) FROM animals WHERE species = 'pokemon';
+
+-- Avg escape attempts for animals born between 1990 and 2000;
+SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth > '1990-01-01' AND date_of_birth < '1999-12-31';
